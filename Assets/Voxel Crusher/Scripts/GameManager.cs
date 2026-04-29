897eed10ec3d1cf0c61b15c9de0fe217c30f7109
TeT1991 <73365839+TeT1991@users.noreply.github.com>
2026-04-29 14:56:25 +0300

    End level after no income timeout

--- Assets/Voxel Crusher/Scripts/GameManager.cs
+++ Assets/Voxel Crusher/Scripts/GameManager.cs
@@ -5,14 +5,22 @@ using JetSystems;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private float noIncomeEndDelay = 15f;
+
+    private int lastCoins;
+    private float noIncomeTimer;
+    private bool levelEndScheduled;
+
     private void Awake()
     {
         RobotFuel.OnFuelEmpty += FuelEmptyCallback;
+        UIManager.onGameSet += ResetNoIncomeTimer;
     }
 
     private void OnDestroy()
     {
         RobotFuel.OnFuelEmpty -= FuelEmptyCallback;
+        UIManager.onGameSet -= ResetNoIncomeTimer;
     }
 
     // Start is called before the first frame update
@@ -24,14 +32,38 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!UIManager.IsGame() || levelEndScheduled)
+            return;
+
+        if (UIManager.COINS > lastCoins)
+        {
+            lastCoins = UIManager.COINS;
+            noIncomeTimer = 0;
+            return;
+        }
+
+        noIncomeTimer += Time.deltaTime;
+
+        if (noIncomeTimer >= noIncomeEndDelay)
+            FuelEmptyCallback();
     }
 
     public void FuelEmptyCallback()
     {
+        if (levelEndScheduled)
+            return;
+
+        levelEndScheduled = true;
         LeanTween.delayedCall(1, () => UIManager.setLevelCompleteDelegate?.Invoke());
 
 
         Debug.Log("Setting level complete...");
+    }
+
+    private void ResetNoIncomeTimer()
+    {
+        lastCoins = UIManager.COINS;
+        noIncomeTimer = 0;
+        levelEndScheduled = false;
     }
 }
